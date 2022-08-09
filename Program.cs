0fe44995eeb8f801862db7b6d2c2f4aa024c9a46
Max <maxlin2905@gmail.com>
2022-08-09 10:29:37 +1000

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reflection_and_delegate_demo1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World!");
+            }
+        }
+    }
